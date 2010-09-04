70dba0b0e06fa8dd7ba4852d94460eb5e13f0bf8
HugoLnx <hugolnx@gmail.com>
2010-09-03 21:44:47 -0300

    mudando o desenho da estrela de um anel para um tipo de brilho

--- javascript/Estrela.js
+++ javascript/Estrela.js
@@ -5,27 +5,42 @@ function Estrela(r,x_inicial,y_inicial){
 	var cor = randomizarCor();
 	
 	function randomizarCor(){
-		var cor = '#'
-		var qnt_f = 0;
-		var qnt_zero = 0;
+		cor = [];
+		qnt_f = 0;
+		qnt_zero = 0;
 		for (var i=0;i<3;i+=1){
 			if ((qnt_f != 2 && Math.floor(Math.random()*10) % 2 == 0) || qnt_zero == 2){
-				cor += 'ff';
+				cor.push(255);
 				qnt_f += 1;
 			} else {
-				cor += '00';
+				cor.push(0);
 				qnt_zero += 1;
 			}
 		}
 		return cor;
 	}
 	
+	function stringCorComTransparencia(alpha){
+		return "rgba("+cor[0]+","+cor[1]+","+cor[2]+","+alpha+")";
+	}
+	
 	this.se_desenhar = function(context){
-		context.strokeStyle = cor;
-		context.beginPath();
-		context.arc(x,y,raio,0,Math.PI*2,true);
-		context.stroke();
-		context.closePath();
+		for (var i = 1; i <= raio; i++){
+			if (i < raio * 0.1)
+				var alpha = 1
+			else if (i < raio * 0.333)
+				var alpha = 0.3
+			else if (i < raio * 0.5)
+				var alpha = 0.3
+			else
+				var alpha = 0.1
+
+			context.fillStyle = stringCorComTransparencia(alpha);
+			context.beginPath();
+			context.arc(x,y,i,0,Math.PI*2,true);
+			context.closePath();
+			context.fill();
+		}
 	}
 	
 	this.copiarPosicao = function(estrela){
