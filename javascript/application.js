96fc2f0ef194fc5a5e5211ebc775dae34fd7366f
HugoLnx <hugolnx@gmail.com>
2011-02-14 12:52:40 -0200

    refatoracao

--- javascript/application.js
+++ javascript/application.js
@@ -2,16 +2,16 @@ var canvas;
 var mouseEvent;
 
 function init(){
-	document.onmousemove = onCanvasMouseMove;
-  window.setInterval(onActEstrela,20);
+	document.onmousemove = guardarMouseEvent;
+	window.setInterval(atualizarEstrela,20);
 	canvas = new Canvas('estrelas');
 }
 
-function onCanvasMouseMove(e){
-  mouseEvent = e;
+function guardarMouseEvent(e){
+	mouseEvent = e;
 }
 
-function onActEstrela(){
-  if (mouseEvent != undefined)
-	  canvas.atualizar(mouseEvent.pageX,mouseEvent.pageY);
+function atualizarEstrela(){
+	if (mouseEvent != undefined)
+		canvas.atualizar(mouseEvent.pageX,mouseEvent.pageY);
 }
