be2e86835595a2f36b4689454586484df694b15b
HugoLnx <hugolnx@gmail.com>
2011-02-14 10:57:25 -0200

    atualizacao do brilho independente do movimento do mouse

--- javascript/application.js
+++ javascript/application.js
@@ -1,10 +1,17 @@
 var canvas;
+var mouseEvent;
 
 function init(){
 	document.onmousemove = onCanvasMouseMove;
+  window.setInterval(onActEstrela,20);
 	canvas = new Canvas('estrelas');
 }
 
 function onCanvasMouseMove(e){
-	canvas.atualizar(e.pageX,e.pageY);
+  mouseEvent = e;
+}
+
+function onActEstrela(){
+  if (mouseEvent != undefined)
+	  canvas.atualizar(mouseEvent.pageX,mouseEvent.pageY);
 }
